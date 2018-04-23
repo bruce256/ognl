917b932cd37a9b35792a89ccbded9415ea24b7ed
lvsheng <lvsheng.ls@alibaba-inc.com>
2018-04-23 12:09:54 +0800

    fel

---
+++ src/main/java/com/alibaba/ls/FelTest.java
@@ -0,0 +1,29 @@
+package com.alibaba.ls;
+
+import com.greenpineyu.fel.FelEngine;
+import com.greenpineyu.fel.FelEngineImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @auther 儒尊
+ * @date 2018/4/23
+ **/
+public class FelTest {
+	
+	public static void main(String[] args) {
+		List list = new ArrayList();
+		for(int i=0;i<10000; i++) {
+			FelEngine felEngine = new FelEngineImpl();
+			list.add(felEngine);
+		}
+		
+		System.out.println("end");
+		try {
+			Thread.sleep(100000000L);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
